c7382687c82f9b55c4d7e6d8284c8a5d030eb1ec
claybowl <claybowlproductions@gmail.com>
2023-03-22 22:41:37 -0500

    task 9 attempt

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: 
+ *
+ * Return: Height of the tree, 0 if tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (++left_height);
+	return (++right_height);
+}
