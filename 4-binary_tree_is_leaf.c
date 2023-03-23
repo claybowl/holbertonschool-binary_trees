db4c53cec4fde1bf9bd37ad2af7403d01db2b584
claybowl <claybowlproductions@gmail.com>
2023-03-22 22:22:20 -0500

    task 5 attempt

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -4,6 +4,8 @@
  * binary_tree_is_leaf - Checks if a node is a leaf
  *
  * Return: 1 if node is a leaf, otherwise 0
+ * 
+ * @node: pointer to the node to check
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
