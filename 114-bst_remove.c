9a6a5a08a59a1137af90b7440d047e0c5bf3678c
claybowl <claybowlproductions@gmail.com>
2023-04-05 22:27:40 -0500

    task 4 attempt

---
+++ 114-bst_remove.c
@@ -0,0 +1,83 @@
+#include "binary_trees.h"
+#include <limits.h>
+
+/**
+ * bst_search - Searches for a value in a Binary Search Tree
+ *
+ * @tree: Pointer to the root node of the BST to search
+ * @value: Value to search in the tree
+ *
+ * Return: Pointer to the node containing a value equal to 'value'
+ * If tree is NULL or if nothing is found, return NULL
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree == NULL)
+	{
+		return NULL;
+	}
+
+	if (tree->n == value)
+	{
+		return (bst_t *)tree;
+	}
+	else if (tree->n > value)
+	{
+		return bst_search(tree->left, value);
+	}
+	else
+	{
+		return bst_search(tree->right, value);
+	}
+}
+
+/**
+ * bst_remove - Removes a node from a Binary Search Tree
+ *
+ * @root: Pointer to the root node of the BST to search
+ * @value: Value to remove from the tree
+ *
+ * Return: Pointer to the new root node of the tree after
+ * removing the desired value
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *temp;
+
+	if (!root)
+		return (NULL);
+
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		if (!root->left && !root->right)
+		{
+			free(root);
+			return (NULL);
+		}
+		else if (!root->left)
+		{
+			temp = root->right;
+			free(root);
+			return (temp);
+		}
+		else if (!root->right)
+		{
+			temp = root->left;
+			free(root);
+			return (temp);
+		}
+		else
+		{
+			for (temp = root->right; temp->left; temp = temp->left)
+			{
+			}
+			root->n = temp->n;
+			root->right = bst_remove(root->right, temp->n);
+		}
+	}
+	return (root);
+}
