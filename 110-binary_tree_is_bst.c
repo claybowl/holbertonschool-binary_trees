b84a886f64fb76ab6436d9f8a594e4c9bed82aa1
claybowl <claybowlproductions@gmail.com>
2023-04-05 20:04:45 -0500

    fixed betty

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -14,12 +14,12 @@ int is_bst_helper(const binary_tree_t *tree, int min, int max)
 {
 	if (tree == NULL)
 	{
-		return 1;
+		return (1);
 	}
 
 	if (tree->n < min || tree->n > max)
 	{
-		return 0;
+		return (0);
 	}
 
 	return is_bst_helper(tree->left, min, tree->n - 1) &&
@@ -37,7 +37,7 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 	{
-		return 0;
+		return (0);
 	}
 	return is_bst_helper(tree, INT_MIN, INT_MAX);
 }
