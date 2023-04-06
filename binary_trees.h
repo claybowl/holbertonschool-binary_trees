1373d8fa25339bd94a1472f53a58209d48bec7cf
claybowl <claybowlproductions@gmail.com>
2023-04-05 19:47:28 -0500

    task 0 attempt

--- binary_trees.h
+++ binary_trees.h
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <limits.h>
 
 /* Structure */
 
@@ -27,6 +29,7 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
 
 /* Prototypes */
 
@@ -51,7 +54,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
+int is_bst_helper(const binary_tree_t *tree, int min, int max);
+int binary_tree_is_bst(const binary_tree_t *tree);
 int is_perfect_recursive(const binary_tree_t *tree, int depth, int level);
 
 #endif
