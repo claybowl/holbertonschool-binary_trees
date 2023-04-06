260c2254634d5a647b47d9a7af82890219205157
claybowl <claybowlproductions@gmail.com>
2023-04-05 22:36:54 -0500

    task 4 attempt...

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -33,29 +33,28 @@ bst_t *bst_search(const bst_t *tree, int value)
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *temp;
+	bst_t *temp = NULL;
 
-	if (!root)
+	if (root == NULL)
 		return (NULL);
-
-	if (value < root->n)
+	else if (value < root->n)
 		root->left = bst_remove(root->left, value);
 	else if (value > root->n)
 		root->right = bst_remove(root->right, value);
 	else
 	{
-		if (!root->left && !root->right)
+		if (root->left == NULL && root->right == NULL)
 		{
 			free(root);
 			return (NULL);
 		}
-		else if (!root->left)
+		else if (root->left == NULL)
 		{
 			temp = root->right;
 			free(root);
 			return (temp);
 		}
-		else if (!root->right)
+		else if (root->right == NULL)
 		{
 			temp = root->left;
 			free(root);
@@ -63,11 +62,12 @@ bst_t *bst_remove(bst_t *root, int value)
 		}
 		else
 		{
-			for (temp = root->right; temp->left; temp = temp->left)
-			{
-			}
+			temp = root->right;
+			while (temp->left != NULL)
+				temp = temp->left;
 			root->n = temp->n;
 			root->right = bst_remove(root->right, temp->n);
+			return (root);
 		}
 	}
 	return (root);
