e737b18b4f8a5014d2a2143002121ce0e196088f
claybowl <claybowlproductions@gmail.com>
2023-04-05 21:22:36 -0500

    fixed?

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -4,26 +4,43 @@
 /**
  * bst_insert - Inserts a value in a Binary Search Tree
  *
+ *
  * @tree: double pointer to the root node of the BST to insert the value
  * @value: value to store in the node to be inserted
  *
  * Return: success: pointer to the created node, failure: NULL
  */
 bst_t *bst_insert(bst_t **tree, int value)
-
 {
+	bst_t *new_node = NULL;
 
 	if (tree == NULL)
 		return (NULL);
-
 	if (*tree == NULL)
-		return (*tree = binary_tree_node(NULL, value));
-
+	{
+		new_node = binary_tree_node(*tree, value);
+		*tree = new_node;
+		return (new_node);
+	}
 	if (value < (*tree)->n)
+	{
+		if ((*tree)->left == NULL)
+		{
+			new_node = binary_tree_node(*tree, value);
+			(*tree)->left = new_node;
+			return (new_node);
+		}
 		return (bst_insert(&(*tree)->left, value));
-
+	}
 	if (value > (*tree)->n)
-
+	{
+		if ((*tree)->right == NULL)
+		{
+			new_node = binary_tree_node(*tree, value);
+			(*tree)->right = new_node;
+			return (new_node);
+		}
 		return (bst_insert(&(*tree)->right, value));
+	}
 	return (NULL);
 }
