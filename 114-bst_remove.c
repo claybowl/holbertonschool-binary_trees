7fea3727484f5595113608cbc11aa52d03972931
claybowl <claybowlproductions@gmail.com>
2023-04-05 22:32:32 -0500

    task 4 attempt

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -13,22 +13,13 @@
 bst_t *bst_search(const bst_t *tree, int value)
 {
 	if (tree == NULL)
-	{
-		return NULL;
-	}
-
+		return (NULL);
 	if (tree->n == value)
-	{
-		return (bst_t *)tree;
-	}
+		return ((bst_t *)tree);
 	else if (tree->n > value)
-	{
-		return bst_search(tree->left, value);
-	}
+		return (bst_search(tree->left, value));
 	else
-	{
-		return bst_search(tree->right, value);
-	}
+		return (bst_search(tree->right, value));
 }
 
 /**
